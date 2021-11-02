610cff2f029881916b3ce936e0fb79bd6b0972cb
zerbash <dbasham@overlake.org>
2021-11-02 13:31:09 -0700

    11-2-21

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -1,17 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class WordCounter {
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("input file");
         Scanner console = new Scanner(System.in);
         Scanner scanner = new Scanner(new File(console.next()));
-        Set<String> words = new HashSet<>();
+        Map<String, Integer> words = new HashMap<>();
         while (scanner.hasNext()) {
-            words.add(scanner.next());
+            String word = scanner.next();
+            if (words.containsKey(word)) {
+                words.put(word, words.get(word) + 1);
+            } else {
+                words.put(word, 1);
+            }
         }
         System.out.println("# of unique words: " + words.size());
 
